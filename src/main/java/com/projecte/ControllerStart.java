a90eff8e0e6b986797efd17b8f9c5387c796a599
Revers <victorpg182006@gmail.com>
2025-04-22 20:28:05 +0200

    initialize

---
+++ src/main/java/com/projecte/ControllerStart.java
@@ -0,0 +1,14 @@
+package com.projecte;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ControllerStart implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
--- src/main/java/com/projectepokemon/Main.java
+++
@@ -1,7 +0,0 @@
-package com.projectepokemon;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
