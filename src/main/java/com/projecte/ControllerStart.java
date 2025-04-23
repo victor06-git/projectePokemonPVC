4fd369df8a11d4a65249b3353d2bff4bc05e102e
xhector7 <150951023+xhector7@users.noreply.github.com>
2025-04-23 19:23:22 +0200

    Avanços basics en el ControllerStart i Main

--- src/main/java/com/projecte/ControllerStart.java
+++ src/main/java/com/projecte/ControllerStart.java
@@ -3,12 +3,25 @@ package com.projecte;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 
 public class ControllerStart implements Initializable {
 
 
-    @Override
+        @Override
     public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Pantalla de inicio cargada");
+    }
+
+    @FXML
+    private void empezarJuego() {
+        System.out.println("Boton Open Game pulsado");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Inicio del Juego");
+        alert.setHeaderText(null);
+        alert.setContentText("Aqui comenzaría el juego o se cambiaria de escena.");
+        alert.showAndWait();
     }
 }
