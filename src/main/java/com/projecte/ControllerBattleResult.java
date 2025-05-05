adcd6aa17d74ac9f4863628e779b10e152bd695f
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-05-05 17:12:56 +0200

    cambios en el controlador de battle result

--- src/main/java/com/projecte/ControllerBattleResult.java
+++ src/main/java/com/projecte/ControllerBattleResult.java
@@ -3,9 +3,38 @@ package com.projecte;
 import java.io.File;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
 
 public class ControllerBattleResult {
+
+    @FXML
+    private Button btnMenu = new Button();
+
+    @FXML
+    private Label LabelNivel;
+
+    @FXML
+    private Label LabelGanador, LaNivel, LabelNpokemons, LabelItems;
+
+    @FXML
+    private TextField NomGanador, LevelActual, levelAumentat, NewsPokemons = new TextField();
+    
+    @FXML
+    private TextArea itemsRecogidos = new TextArea();
+
+    @FXML
+    private ProgressBar BarraProg = new ProgressBar();
+
+    // Método que se llama al inicializar el controlador
+    @FXML
+    public void initialize() {
+     
+    }
     
     
 }
