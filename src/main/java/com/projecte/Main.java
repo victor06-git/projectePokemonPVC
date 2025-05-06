6b559d56122707a79ae86b16b7d5f964a2de7984
Revers <victorpg182006@gmail.com>
2025-05-06 17:40:13 +0200

    .

--- src/main/java/com/projecte/Main.java
+++ src/main/java/com/projecte/Main.java
@@ -20,11 +20,6 @@ public class Main extends Application {
         // Carga vistas
         UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
         UtilsViews.addView(getClass(), "ViewStart", "/assets/viewStart.fxml");
-<<<<<<< HEAD
-        UtilsViews.addView(getClass(), "ViewMenu", "/assets/viewMenu.fxml");
-
-
-=======
         UtilsViews.addView(getClass(), "ViewPokeSettings", "/assets/viewPokeSettings.fxml");
         UtilsViews.addView(getClass(), "ViewBattleAttack", "/assets/viewBattleAttack.fxml");
         UtilsViews.addView(getClass(), "pokemonView", "/assets/pokemonView.fxml");
@@ -33,7 +28,6 @@ public class Main extends Application {
 
         UtilsViews.setView("ViewBattleOptions");
         // Mostrar la finestra
->>>>>>> 223cae3f978c513d783b46f6a7a3f769027d549b
         Scene scene = new Scene(UtilsViews.parentContainer);
         stage.setScene(scene);
         stage.setTitle("Pokémons PvP");
