34de5eb05b91449008ac397bd0ade59ac10cf0b0
Revers <victorpg182006@gmail.com>
2025-05-08 17:57:10 +0200

    .

--- src/main/java/com/projecte/Main.java
+++ src/main/java/com/projecte/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        
 
         // Carga vistas
         UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
