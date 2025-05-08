def3c2eb76d315741fefa53977c342fc56c204dd
Revers <victorpg182006@gmail.com>
2025-05-08 17:49:22 +0200

    .

--- src/main/java/com/projecte/ControllerMenu.java
+++ src/main/java/com/projecte/ControllerMenu.java
@@ -1,35 +1,34 @@
 package com.projecte;
 
-import java.net.URL;
-import java.nio.file.Path;
-import java.util.ResourceBundle;
+
+import com.utils.UtilsViews;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class ControllerMenu implements Initializable {
-
-    @FXML
-    private ImageView imgArrowBack;
+public class ControllerMenu {
 
     @FXML
-    private ImageView imgStart;
+    private Label levelInfoLabel, pokemonsCaughtInfoLabel, pointsInfoLabel, battlesWonInfoLabel;
 
     @FXML
-    private ImageView imgExit;
+    private Button managementButton, battleHistoryButton, newBattleButton, exitButton;
 
-   @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        Path imagePath = null;
-        try {
-            URL imageURL = getClass().getResource("/assets/image/arrow-back.gif");
-            Image image = new Image(imageURL.toExternalForm());
-            imgArrowBack.setImage(image);
-        } catch (Exception e) {
-            System.err.println("Error loading image asset: " + imagePath);
-            e.printStackTrace();
-        }
+    public void toViewManagement() {
+        UtilsViews.setViewAnimating("ViewManagement");
     }
+    
+    public void toViewBattleHistory() {
+        UtilsViews.setViewAnimating("ViewBattleHistory");
+    }
+    
+    public void toViewNewBattle() {
+        UtilsViews.setViewAnimating("ViewNewBattle");
+    } 
+    
+    public void toViewStart() {
+        UtilsViews.setViewAnimating("ViewStart");
+    }
+
 }
